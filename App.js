71be0d6770ba71ddc32062d5f68d93eab4f69576
gpeggy <163522099+gpeggy@users.noreply.github.com>
2024-05-22 02:01:23 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+export default function App() {
+  const [advice, setAdvice] = useState("");
+  const [count, setCount] = useState(0);
+  async function getAdvice() {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+    setAdvice(data.slip.advice);
+    setCount((c) => c + 0);
+  }
+  useEffect(function () {
+    getAdvice();
+  }, []);
+
+  return (
+    <div>
+      <h1>{advice}</h1>
+      <button onClick={getAdvice}>Get Advice</button>
+      <Message count={count} />
+    </div>
+  );
+}
+function Message(props) {
+  return (
+    <p>
+      You have read <b>{props.count}</b> pieces of advice.
+    </p>
+  );
+}
