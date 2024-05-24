8333d8a19f94661591823c9544ba43732601c419
gpeggy <163522099+gpeggy@users.noreply.github.com>
2024-05-24 20:14:24 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import {useState} from "react";
 export default function App() {
   const [advice, setAdvice] = useState("");
   const [count, setCount] = useState(0);
